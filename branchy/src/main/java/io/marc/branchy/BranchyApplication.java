8d6b81348e0980ca5c0f67ae239bbb0ae18c8834
marcfreir <marcfreir@outlook.com>
2020-08-27 19:40:27 -0200

    Initial commit

---
+++ branchy/src/main/java/io/marc/branchy/BranchyApplication.java
@@ -0,0 +1,13 @@
+package io.marc.branchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchyApplication.class, args);
+	}
+
+}
---
+++ branchy/src/test/java/io/marc/branchy/BranchyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marc.branchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
